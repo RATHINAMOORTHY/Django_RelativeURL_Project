11159a857536c7cacb5c84a6a9ba38d2fd56adde
RATHINAMOORTHY <nrathinamoorthy@gmail.com>
2019-06-30 16:05:52 +0530

    Relative URL Hanson

---
+++ RelativeURL/MyApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'MyApp/index.html')
+
+def about(request):
+    return render(request,'MyApp/about.html')
+
+def careers(request):
+    return render(request,'MyApp/careers.html')
